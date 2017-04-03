02a2365b7462b402a4a141315bc19436cb9b783f
mak001 <mak001001@gmail.com>
2017-04-03 09:25:38 -0500

    Add project files.

---
+++ Hot Ash Cigars/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hot_Ash_Cigars.MvcApplication" Language="C#" %>
---
+++ Hot Ash Cigars/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hot_Ash_Cigars.Startup))]
+namespace Hot_Ash_Cigars
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
